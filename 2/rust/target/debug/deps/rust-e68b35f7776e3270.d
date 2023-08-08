3c7faef72b278507a8b749a8931bd4e6bd4aea24
Matthew Vogel <matthew.scott.vogel@gmail.com>
2023-08-08 21:39:51 +1000

    rust challenge 2

--- 2/rust/target/debug/deps/rust-e68b35f7776e3270.d
+++
@@ -1,5 +0,0 @@
-/Users/matt/Documents/personal/challenges/2/rust/target/debug/deps/rust-e68b35f7776e3270: src/main.rs
-
-/Users/matt/Documents/personal/challenges/2/rust/target/debug/deps/rust-e68b35f7776e3270.d: src/main.rs
-
-src/main.rs:
--- 2/rust/target/debug/rust.d
+++
@@ -1,1 +0,0 @@
-/Users/matt/Documents/personal/challenges/2/rust/target/debug/rust: /Users/matt/Documents/personal/challenges/2/rust/src/main.rs
